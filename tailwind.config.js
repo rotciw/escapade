9011872ac6f60adb6dd9b506ede01f0299af2260
rotciw <wictor_x@hotmail.com>
2022-02-09 13:03:41 +0100

    Add Inter font, and add font and colors to tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    colors: {
+      white: '#FFFFFF',
+      black: '#000000',
+      'alice-blue': '#F1F3F9',
+      'cameo-pink': '#EFBCD5',
+      'colorful-blue': '#5170ED',
+      independence: '#4B5267',
+      'magic-mint': '#BCEFD6',
+    },
+    fontFamily: {
+      inter: 'Inter, sans-serif',
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
