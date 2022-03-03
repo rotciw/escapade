ef4bc9dc76438c7be6c3fb14a2e893f0867f3de4
rotciw <wictor_x@hotmail.com>
2022-03-03 16:54:41 +0100

    Adjust sticky header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ const Header: React.FC = () => {
   const navigate = useNavigate();
   return (
     <>
-      <header className='sticky top-0 flex justify-center '>
+      <header className='top-0 flex justify-center '>
         <h1
           className='py-2 mt-5 text-5xl font-bold text-alice-blue hover:cursor-pointer w-fit'
           onClick={() => navigate('/')}
