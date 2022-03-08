e1d81653ec571fb04d67da7304c1879e0ed57054
Jakob Kielland <jakob-ek@hotmail.com>
2022-03-08 17:57:45 +0100

    setup basic elements and style

--- src/views/host/creatingGameView.tsx
+++ src/views/host/creatingGameView.tsx
@@ -37,9 +37,9 @@ const CreatingGameView: React.FC = () => {
     <>
       <Header />
       {/* Choose a theme */}
-      <div className='flex flex-col items-center'>
-        <h1>Velg et tema</h1>
-        <div className='flex'>
+      <div className='flex flex-col w-2/3 max-w-2xl m-auto items-left '>
+        <h1 className='my-3 text-2xl font-bold'>Velg et tema</h1>
+        <div className='flex justify-between w-full pr-1'>
           <button className='btn-sq'>Intro til Escapade</button>
           <button className='btn-sq'>Revolu-sjonene</button>
           <button className='btn-sq'>1800-tallet</button>
@@ -48,14 +48,66 @@ const CreatingGameView: React.FC = () => {
           {/* More options */}
         </div>
 
-        <h2>Bilder som vil vises</h2>
-        <div className='flex'>
-          <img src='' />
+        <h2 className='mt-8'>Bilder som vil vises</h2>
+        <div className='flex w-full p-2 mb-3 border-base bg-alice-blue'>
+          <img
+            src='https://i.imgur.com/22HFZBt.png'
+            className='box-border object-cover w-1/3 m-2 border-base h-28'
+          />
+          <img
+            src='https://i.imgur.com/22HFZBt.png'
+            className='box-border object-cover w-1/3 m-2 border-base h-28'
+          />
+          <img
+            src='https://i.imgur.com/22HFZBt.png'
+            className='box-border object-cover w-1/3 m-2 border-base h-28'
+          />
         </div>
-        <div>
-          <h1>Flere innstillinger</h1>
+        <h1 className='my-4 text-2xl font-bold'>Flere innstillinger</h1>
+        <div className='p-3 text-sm text-black bg-alice-blue border-base'>
+          <div className='flex justify-between'>
+            <div className='box-border w-1/2 p-4'>
+              <h2 className='text-lg font-semibold'>Tid per runde</h2>
+              <p>Velg varigheten på hver runde</p>
+              <select name='time' id='timeDropdown' className='p-1 shadow-lg h-7 border-base'>
+                <option>2 minutter</option>
+                <option>5 minutter</option>
+                <option selected>10 minutter</option>
+                <option>20 minutter</option>
+              </select>
+            </div>
+            <div className='box-border w-1/2 p-4'>
+              <h2 className='text-lg font-semibold'>Tilfeldige lag</h2>
+              <p>Velg om lag skal velges tilfeldig eller være opp til spillerene</p>
+              <input
+                type='checkbox'
+                id='randomTeamsCheckbox'
+                className='shadow-lg h-7 w-7 border-base'
+              />
+            </div>
+          </div>
+          <div className='flex justify-between'>
+            <div className='box-border w-1/2 p-4'>
+              <h2 className='text-lg font-semibold'>Maksimal lagstørrelse</h2>
+              <p>Velg størrelsen på lag, eller la det styres automatisk av spillet</p>
+              <select name='time' id='timeDropdown' className='p-1 shadow-lg h-7 border-base'>
+                <option>Auto (anbefalt)</option>
+                <option>4</option>
+                <option>5</option>
+              </select>
+            </div>
+            <div className='box-border w-1/2 p-4'>
+              <h2 className='text-lg font-semibold'>Tilfeldige roller</h2>
+              <p>Velg om spillerene kan velge rollene sine eller om de skal velges tilfeldig</p>
+              <input
+                type='checkbox'
+                id='randomRolesCheckbox'
+                className='shadow-lg h-7 w-7 border-base'
+              />
+            </div>
+          </div>
         </div>
-        <button className='my-2 btn-lg' onClick={() => createGame()}>
+        <button className='m-auto mt-8 btn-lg w-44' onClick={() => createGame()}>
           Fortsett
         </button>
       </div>
