a524c061d77cc4fc0c50ce87e632d16211e5c900
rotciw <wictor_x@hotmail.com>
2022-03-16 16:43:17 +0100

    Add temporary progress bar for timer

--- src/views/player/inGameView.tsx
+++ src/views/player/inGameView.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { doc, onSnapshot, updateDoc } from 'firebase/firestore';
+import { doc, onSnapshot } from 'firebase/firestore';
 import { db } from '../../helpers/firebase';
 import { useLocalStorage } from '../../hooks/useLocalStorage';
 import Header from '~/components/header';
@@ -123,16 +122,25 @@ const InGameView: React.FC = () => {
   return (
     <>
       <Header />
-      <div className='flex flex-row flex-wrap pb-8 mt-8 justify-evenly'>
-        <div className='w-3/5 p-2 text-center rounded h-fit bg-alice-blue'>
+      <div className='flex flex-row flex-wrap justify-around pb-8 mt-8'>
+        <div className='w-7/12 p-2 text-center rounded h-fit bg-alice-blue'>
           <Zoom>
             <img src={urlFor(gameData.questionSet[round].images[0].asset).url()} />
           </Zoom>
           <p className='italic text-black'>Klikk på bildet for å zoome!</p>
         </div>
-        <div className='flex flex-col w-2/6 h-full p-4 px-8 pb-8 text-black rounded bg-alice-blue'>
-          <div className='flex flex-col pb-4'>
-            <h1 className='mb-2 text-xl font-semibold text-center'>Runde {round + 1}/3</h1>
+        <div className='flex flex-col w-4/12'>
+          <p className='mb-1 text-lg font-semibold'>Tid igjen:</p>
+          <div className='flex items-center w-full h-10 mb-4 rounded bg-alice-blue-hover align-center'>
+            <div
+              className='h-10 p-3 my-auto text-sm font-semibold leading-none text-center text-black rounded bg-cameo-pink'
+              style={{ width: '5%' }}
+            >
+              09:85
+            </div>
+          </div>
+          <div className='flex flex-col p-4 px-8 pb-8 text-black rounded bg-alice-blue'>
+            <h1 className='mb-2 text-xl font-semibold '>Runde {round + 1}/3</h1>
             <label className='mb-1 font-semibold'>
               1. {gameData.questionSet[round].multipleChoiceQuestion.question}
             </label>
@@ -151,66 +159,67 @@ const InGameView: React.FC = () => {
                 </div>
               );
             })}
-          </div>
-          <div className='flex flex-col pb-2'>
-            <label className='mb-1 font-semibold'>
-              2. {gameData.questionSet[round].stringDateQuestion.question}
-            </label>
-            <input
-              className={`${
-                chosenDate && !dateErrorMsg
-                  ? 'bg-alice-blue-hover'
-                  : 'bg-white hover:bg-alice-blue-hover'
-              } text-center cursor-text text-independence input-main`}
-              type='date'
-              onChange={(e) => handleDate(e)}
-              max='2022-04-14'
-            />
-            {dateErrorMsg ? <p className='italic text-red'>{dateErrorMsg}</p> : <p>&nbsp;</p>}
-          </div>
-          <div className='flex flex-col pt-0'>
-            <label className='mb-1 font-semibold'>
-              3. {gameData.questionSet[round].mapPointerQuestion.question}
-            </label>
 
-            {!isOpen && markerLatitude !== 0 && markerLatitude !== 0 ? (
-              <button
-                className='flex justify-center p-2 italic transition-all border rounded bg-alice-blue-hover'
-                onClick={() => setIsOpen(true)}
-              >
-                Trykk igjen for å endre sted <ExternalLink className='ml-2' />
-              </button>
-            ) : (
-              <button
-                className='flex justify-center p-2 transition-all bg-white border rounded hover:bg-alice-blue'
-                onClick={() => setIsOpen(true)}
-              >
-                Åpne kartet <ExternalLink className='ml-2' />
-              </button>
-            )}
+            <div className='flex flex-col pb-2'>
+              <label className='mb-1 font-semibold'>
+                2. {gameData.questionSet[round].stringDateQuestion.question}
+              </label>
+              <input
+                className={`${
+                  chosenDate && !dateErrorMsg
+                    ? 'bg-alice-blue-hover'
+                    : 'bg-white hover:bg-alice-blue-hover'
+                } text-center cursor-text text-independence input-main`}
+                type='date'
+                onChange={(e) => handleDate(e)}
+                max='2022-04-14'
+              />
+              {dateErrorMsg ? <p className='italic text-red'>{dateErrorMsg}</p> : <p>&nbsp;</p>}
+            </div>
+            <div className='flex flex-col pt-0'>
+              <label className='mb-1 font-semibold'>
+                3. {gameData.questionSet[round].mapPointerQuestion.question}
+              </label>
 
-            <PopUpComponent isOpen={isOpen} openFunction={setIsOpen}>
-              <h1 className='mb-1 text-lg italic font-medium text-center'>
-                Trykk og velg cirka der du tror det er.
-              </h1>
-              <div className='h-[65vh] w-[85vw]'>
-                <GoogleMapReact
-                  yesIWantToUseGoogleMapApiInternals
-                  bootstrapURLKeys={{ key: 'AIzaSyCBtuU2hX_fJLSczcVSSbdze-KcyFhr0IY' }}
-                  defaultCenter={center}
-                  defaultZoom={zoom}
-                  onClick={(e) => onMarkerClick(e)}
-                  options={{ fullscreenControl: false }}
+              {!isOpen && markerLatitude !== 0 && markerLatitude !== 0 ? (
+                <button
+                  className='flex justify-center p-2 italic transition-all border rounded bg-alice-blue-hover'
+                  onClick={() => setIsOpen(true)}
                 >
-                  {markerLatitude !== 0 && markerLongitude !== 0 && (
-                    <Marker lat={markerLatitude} lng={markerLongitude} />
-                  )}
-                </GoogleMapReact>
-              </div>
-            </PopUpComponent>
+                  Trykk igjen for å endre sted <ExternalLink className='ml-2' />
+                </button>
+              ) : (
+                <button
+                  className='flex justify-center p-2 transition-all bg-white border rounded hover:bg-alice-blue'
+                  onClick={() => setIsOpen(true)}
+                >
+                  Åpne kartet <ExternalLink className='ml-2' />
+                </button>
+              )}
+
+              <PopUpComponent isOpen={isOpen} openFunction={setIsOpen}>
+                <h1 className='mb-1 text-lg italic font-medium text-center'>
+                  Trykk og velg cirka der du tror det er.
+                </h1>
+                <div className='h-[65vh] w-[85vw]'>
+                  <GoogleMapReact
+                    yesIWantToUseGoogleMapApiInternals
+                    bootstrapURLKeys={{ key: 'AIzaSyCBtuU2hX_fJLSczcVSSbdze-KcyFhr0IY' }}
+                    defaultCenter={center}
+                    defaultZoom={zoom}
+                    onClick={(e) => onMarkerClick(e)}
+                    options={{ fullscreenControl: false }}
+                  >
+                    {markerLatitude !== 0 && markerLongitude !== 0 && (
+                      <Marker lat={markerLatitude} lng={markerLongitude} />
+                    )}
+                  </GoogleMapReact>
+                </div>
+              </PopUpComponent>
+            </div>
+            <div className='w-full mt-4 mb-6 border-t border-dotted border-independence'></div>
+            <button className='btn-lg'>Send svar</button>
           </div>
-          <div className='w-full mt-4 mb-6 border-t border-dotted border-independence'></div>
-          <button className='btn-lg'>Send svar</button>
         </div>
       </div>
     </>
