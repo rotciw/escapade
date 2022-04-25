abff7f1a7d8da74abb1e4eb72be5671c0a48abe4
itzjacki <31799931+itzjacki@users.noreply.github.com>
2022-04-25 14:01:39 +0200

    add youtube video and map (#40)
    * add youtube video and map
    
    * small changes to video size and other stuff
    
    * combine both role checks into one, no need for two
    
    * change order maps are displayed in
    
    * minor cosmetic tweak
    
    * Change to use youtube from sanity and fix map
    
    * Remove test string
    
    * Add breakpoint to map
    
    Co-authored-by: rotciw <wictor_x@hotmail.com>

--- src/components/expertComponent.tsx
+++ src/components/expertComponent.tsx
@@ -4,12 +4,16 @@ import sanityClient from '~/sanityClient';
 import { SanityWikiBankData } from '~/types';
 import imageUrlBuilder from '@sanity/image-url';
 import TableOfContents from './tableOfContents';
+import MapComponent from './mapComponent';
+import getYouTubeId from 'get-youtube-id';
+import YouTube from 'react-youtube';
 
 interface AnswerProps {
   role: number;
 }
 
 const ExpertComponent: React.FC<AnswerProps> = ({ role }) => {
+  const center = { lat: 50, lng: 0 };
   const [wikiBankData, setWikiBankData] = useState<SanityWikiBankData[]>();
   const [text, setText] = useState(null);
   const [tableOfContents, setTableOfContents] = useState(null);
@@ -55,10 +59,16 @@ const ExpertComponent: React.FC<AnswerProps> = ({ role }) => {
         return <h1 className='mt-2 text-2xl font-bold'>{children}</h1>;
       },
       h2: ({ children }: any) => <h1 className='mt-2 text-xl font-bold'>{children}</h1>,
+      h3: ({ children }: any) => <h1 className='mt-1 font-bold text-l'>{children}</h1>,
       normal: ({ children }: any) => <p className='my-2'>{children}</p>,
     },
     types: {
       image: SampleImageComponent,
+      youtube: ({ value }: any) => {
+        const { url } = value;
+        const id = getYouTubeId(url);
+        return <YouTube videoId={id as string} />;
+      },
     },
     marks: {
       // This mark from Sanity gives the corresponding ID to scroll to
@@ -76,7 +86,15 @@ const ExpertComponent: React.FC<AnswerProps> = ({ role }) => {
       <div className='md:w-1/4 mx-auto w-[95vw] mb-4 md:mr-4'>
         <TableOfContents tableOfContents={tableOfContents} />
       </div>
-      <div className='md:w-3/4 w-[95vw] overflow-y-auto flex flex-col pt-5 px-8 pb-8 bg-alice-blue mx-auto max-h-full rounded text-black md:h-[75vh]'>
+      <div className='md:w-3/4 w-[95vw] overflow-y-auto pt-5 px-8 pb-8 bg-alice-blue mx-auto max-h-full rounded text-black md:h-[75vh]'>
+        {role == 2 && (
+          <>
+            <h1 className='text-2xl font-bold'>Kart</h1>
+            <div className='h-[20%] md:h-full mb-6 '>
+              <MapComponent center={center} />
+            </div>
+          </>
+        )}
         <PortableText value={text} components={components} />
       </div>
     </div>
